2777e9c1b4eb496af85978214bf1ae4649f6b190
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2025-09-19 00:06:20 -0400

    feat: Slate theme (#3)

---
+++ internal/app/slatetheme_command.go
@@ -0,0 +1,33 @@
+package app
+
+import (
+	"github.com/eu-ge-ne/toy2/internal/key"
+	"github.com/eu-ge-ne/toy2/internal/palette"
+	"github.com/eu-ge-ne/toy2/internal/theme"
+)
+
+type SlateThemeCommand struct {
+	app    *App
+	option *palette.Option
+}
+
+func NewSlateThemeCommand(app *App) *SlateThemeCommand {
+	return &SlateThemeCommand{
+		app:    app,
+		option: palette.NewOption("Theme Slate", "Theme: Slate", []key.Key{}),
+	}
+}
+
+func (c *SlateThemeCommand) Option() *palette.Option {
+	return c.option
+}
+
+func (c *SlateThemeCommand) Match(key key.Key) bool {
+	return false
+}
+
+func (c *SlateThemeCommand) Run() {
+	c.app.SetColors(theme.Slate{})
+
+	c.app.Render()
+}
---
+++ internal/theme/slate.go
@@ -0,0 +1,60 @@
+package theme
+
+import (
+	"github.com/eu-ge-ne/toy2/internal/vt"
+)
+
+type Slate struct {
+}
+
+func (Slate) DangerBg() []byte {
+	return vt.CharBg(red_900)
+}
+
+func (Slate) MainBg() []byte {
+	return vt.CharBg(slate_900)
+}
+
+func (Slate) MainFg() []byte {
+	return vt.CharFg(slate_900)
+}
+
+func (Slate) Light2Bg() []byte {
+	return vt.CharBg(slate_500)
+}
+
+func (Slate) Light2Fg() []byte {
+	return vt.CharFg(slate_100)
+}
+
+func (Slate) Light1Bg() []byte {
+	return vt.CharBg(slate_700)
+}
+
+func (Slate) Light1Fg() []byte {
+	return vt.CharFg(slate_200)
+}
+
+func (Slate) Light0Bg() []byte {
+	return vt.CharBg(slate_800)
+}
+
+func (Slate) Light0Fg() []byte {
+	return vt.CharFg(slate_300)
+}
+
+func (Slate) Dark0Bg() []byte {
+	return vt.CharBg(slate_950)
+}
+
+func (Slate) Dark0Fg() []byte {
+	return vt.CharFg(slate_400)
+}
+
+func (Slate) Dark1Fg() []byte {
+	return vt.CharFg(slate_600)
+}
+
+func (Slate) Dark2Fg() []byte {
+	return vt.CharFg(slate_700)
+}
