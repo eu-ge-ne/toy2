cbfd159d86cdf078da364cf897e73593130e1f82
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-09-20 21:54:13 -0400

    1

--- internal/saveas/saveas.go
+++ internal/saveas/saveas.go
@@ -2,6 +2,7 @@ package saveas
 
 import (
 	"github.com/eu-ge-ne/toy2/internal/editor"
+	"github.com/eu-ge-ne/toy2/internal/std"
 	"github.com/eu-ge-ne/toy2/internal/theme"
 	"github.com/eu-ge-ne/toy2/internal/ui"
 	"github.com/eu-ge-ne/toy2/internal/vt"
@@ -27,6 +28,50 @@ func (sv *SaveAs) SetColors(t theme.Tokens) {
 	sv.colorText = append(t.Light1Bg(), t.Light1Fg()...)
 
 	sv.editor.SetColors(t)
+}
+
+func (sv *SaveAs) Layout(a ui.Area) {
+	w := std.Clamp(60, 0, a.W)
+	h := std.Clamp(10, 0, a.H)
+
+	sv.area = ui.Area{
+		Y: a.Y + (a.H-h)/2,
+		X: a.X + (a.W-w)/2,
+		W: w,
+		H: h,
+	}
+
+	sv.editor.Layout(ui.Area{
+		Y: sv.area.Y + 4,
+		X: sv.area.X + 2,
+		W: sv.area.W - 4,
+		H: 1,
+	})
+}
+
+func (sv *SaveAs) Render() {
+	if !sv.enabled {
+		return
+	}
+
+	vt.Sync.Bsu()
+
+	vt.Buf.Write(vt.HideCursor)
+	vt.Buf.Write(sv.colorBackground)
+	vt.ClearArea(vt.Buf, sv.area)
+	vt.SetCursor(vt.Buf, sv.area.Y+1, sv.area.X)
+	vt.Buf.Write(sv.colorText)
+	span := sv.area.W
+	vt.WriteTextCenter(vt.Buf, &span, "Save As")
+	vt.SetCursor(vt.Buf, sv.area.Y+sv.area.H-2, sv.area.X)
+	span = sv.area.W
+	vt.WriteTextCenter(vt.Buf, &span, "ESC‧cancel    ENTER‧ok")
+
+	sv.editor.Render()
+
+	vt.Buf.Flush()
+
+	vt.Sync.Esu()
 }
 
 func (sv *SaveAs) Open(filePath string, done chan<- string) {
