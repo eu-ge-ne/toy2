336cd01e4b5966a44dd4bfdd49e83101ff4a2a51
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-09-20 21:50:25 -0400

    1

--- internal/footer/footer.go
+++ internal/footer/footer.go
@@ -3,8 +3,10 @@ package footer
 import (
 	"fmt"
 
+	"github.com/eu-ge-ne/toy2/internal/std"
 	"github.com/eu-ge-ne/toy2/internal/theme"
 	"github.com/eu-ge-ne/toy2/internal/ui"
+	"github.com/eu-ge-ne/toy2/internal/vt"
 )
 
 type Footer struct {
@@ -23,6 +25,37 @@ func New() *Footer {
 func (f *Footer) SetColors(t theme.Tokens) {
 	f.colorBackground = t.Dark0Bg()
 	f.colorText = append(t.Dark0Bg(), t.Dark0Fg()...)
+}
+
+func (f *Footer) Layout(a ui.Area) {
+	f.area = ui.Area{
+		Y: a.Y + a.H - 1,
+		X: a.X,
+		W: a.W,
+		H: std.Clamp(1, 0, a.H),
+	}
+}
+
+func (f *Footer) Render() {
+	if !f.Enabled {
+		return
+	}
+
+	vt.Sync.Bsu()
+
+	vt.Buf.Write(vt.HideCursor)
+	vt.Buf.Write(vt.SaveCursor)
+	vt.Buf.Write(f.colorBackground)
+	vt.ClearArea(vt.Buf, f.area)
+	vt.SetCursor(vt.Buf, f.area.Y, f.area.X)
+	vt.Buf.Write(f.colorText)
+	fmt.Fprintf(vt.Buf, "%*s", f.area.W, f.cursorStatus)
+	vt.Buf.Write(vt.RestoreCursor)
+	vt.Buf.Write(vt.ShowCursor)
+
+	vt.Buf.Flush()
+
+	vt.Sync.Esu()
 }
 
 func (f *Footer) SetCursorStatus(ln0, col0, lnCount int) {
