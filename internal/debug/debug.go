ba6944457ac8e70ce8d728476e9fb70eef5287af
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-09-20 21:44:51 -0400

    1

--- internal/debug/debug.go
+++ internal/debug/debug.go
@@ -1,10 +1,13 @@
 package debug
 
 import (
+	"fmt"
 	"time"
 
+	"github.com/eu-ge-ne/toy2/internal/std"
 	"github.com/eu-ge-ne/toy2/internal/theme"
 	"github.com/eu-ge-ne/toy2/internal/ui"
+	"github.com/eu-ge-ne/toy2/internal/vt"
 )
 
 type Debug struct {
@@ -25,6 +28,46 @@ func New() *Debug {
 func (d *Debug) SetColors(t theme.Tokens) {
 	d.colorBackground = t.Light0Bg()
 	d.colorText = append(t.Light0Bg(), t.Dark0Fg()...)
+}
+
+func (d *Debug) Layout(a ui.Area) {
+	w := std.Clamp(30, 0, a.W)
+	h := std.Clamp(7, 0, a.H)
+
+	d.area = ui.Area{
+		Y: a.Y + a.H - h,
+		X: a.X + a.W - w,
+		W: w,
+		H: h,
+	}
+}
+
+func (d *Debug) Render() {
+	if !d.enabled {
+		return
+	}
+
+	vt.Sync.Bsu()
+
+	vt.Buf.Write(vt.HideCursor)
+	vt.Buf.Write(vt.SaveCursor)
+	vt.Buf.Write(d.colorBackground)
+	vt.ClearArea(vt.Buf, d.area)
+	vt.Buf.Write(d.colorText)
+	vt.SetCursor(vt.Buf, d.area.Y+1, d.area.X+1)
+	fmt.Fprintf(vt.Buf, "Input    : %v", d.inputTime)
+	vt.SetCursor(vt.Buf, d.area.Y+2, d.area.X+1)
+	fmt.Fprintf(vt.Buf, "Render   : %v", d.renderTime)
+	vt.Buf.Write(vt.RestoreCursor)
+	vt.Buf.Write(vt.ShowCursor)
+
+	vt.Buf.Flush()
+
+	vt.Sync.Esu()
+}
+
+func (d *Debug) ToggleEnabled() {
+	d.enabled = !d.enabled
 }
 
 func (d *Debug) SetInputTime(elapsed time.Duration) {
