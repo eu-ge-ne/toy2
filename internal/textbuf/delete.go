ddf4336197fb70016987de8a9fd38e37af0bc502
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-09-23 23:18:19 -0400

    1

--- internal/textbuf/delete.go
+++ internal/textbuf/delete.go
@@ -28,7 +28,7 @@ func (tb *TextBuf) Delete(start int, end int) {
 			node.Bubble(n)
 		} else {
 			y := tb.content.Split(n, offset, count)
-			tb.tree.InsertAfter(n, &y)
+			tb.tree.InsertAfter(n, y)
 		}
 	} else {
 		x := n
@@ -36,14 +36,14 @@ func (tb *TextBuf) Delete(start int, end int) {
 
 		if offset != 0 {
 			y := tb.content.Split(n, offset, 0)
-			tb.tree.InsertAfter(n, &y)
-			x = &y
+			tb.tree.InsertAfter(n, y)
+			x = y
 		}
 
 		lastNode, lastOffset := tb.tree.Root.Find(end)
 		if lastNode != nil && lastOffset != 0 {
 			y := tb.content.Split(lastNode, lastOffset, 0)
-			tb.tree.InsertAfter(lastNode, &y)
+			tb.tree.InsertAfter(lastNode, y)
 		}
 
 		for x != node.NIL && (i < count) {
