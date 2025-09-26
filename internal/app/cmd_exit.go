d0e9dd81719cf30f422e651d31e357a7c780e673
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-09-26 04:01:30 -0400

    1

--- internal/app/cmd_exit.go
+++ internal/app/cmd_exit.go
@@ -28,12 +28,13 @@ func (c *ExitCommand) Match(key key.Key) bool {
 func (c *ExitCommand) Run() {
 	c.app.editor.Enabled = false
 
-	askResult := make(chan bool)
-	go c.app.ask.Open("Save changes?", askResult)
-	save := <-askResult
+	if !c.app.editor.History.IsEmpty() {
+		askResult := make(chan bool)
+		go c.app.ask.Open("Save changes?", askResult)
 
-	if save {
-		c.app.save()
+		if <-askResult {
+			c.app.save()
+		}
 	}
 
 	c.app.exit()
