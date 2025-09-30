c1c0071e88624ea90054b8ed2219e2aa832088e7
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-09-30 03:25:23 -0400

    feat: TOP/BOTTOM

---
+++ internal/editor/h_top.go
@@ -0,0 +1,21 @@
+package editor
+
+import (
+	"github.com/eu-ge-ne/toy2/internal/key"
+)
+
+type TopHandler struct {
+	editor *Editor
+}
+
+func (h *TopHandler) Match(key key.Key) bool {
+	return key.Name == "UP" && key.Super
+}
+
+func (h *TopHandler) Handle(key key.Key) bool {
+	if !h.editor.multiLine {
+		return false
+	}
+
+	return h.editor.Cursor.Top(key.Shift)
+}
