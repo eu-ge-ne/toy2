c767218adebcaf6cdff1e10a28daae0c5922a036
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-09-30 01:22:06 -0400

    feat: Copy/Cut/Paste

---
+++ internal/editor/h_copy.go
@@ -0,0 +1,17 @@
+package editor
+
+import (
+	"github.com/eu-ge-ne/toy2/internal/key"
+)
+
+type CopyHandler struct {
+	editor *Editor
+}
+
+func (h *CopyHandler) Match(key key.Key) bool {
+	return key.Name == "c" && (key.Ctrl || key.Super)
+}
+
+func (h *CopyHandler) Handle(key key.Key) bool {
+	return h.editor.Copy()
+}
