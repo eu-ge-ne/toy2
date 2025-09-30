60f32f1c299e301c21b36ee709c51a111c1893e6
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-09-30 04:17:53 -0400

    1

--- internal/vt/read.go
+++ internal/vt/read.go
@@ -3,11 +3,16 @@ package vt
 import (
 	"bytes"
 	"os"
+	"regexp"
+	"strconv"
 
 	"github.com/eu-ge-ne/toy2/internal/key"
 )
 
+var re = regexp.MustCompile(`\x1b\[\d+;(\d+)R`)
+
 var Keys = make(chan key.Key, 1)
+var Pos = make(chan int)
 
 func Read() {
 	var buf = make([]byte, 1024)
@@ -25,6 +30,15 @@ func Read() {
 				Keys <- key
 				raw = raw[bytesParsed:]
 				continue
+			}
+
+			if match := re.FindStringSubmatch(string(raw)); match != nil {
+				x, err := strconv.Atoi(match[1])
+				if err != nil {
+					panic(err)
+				}
+				Pos <- x - 1
+				break
 			}
 
 			if next_esc_i := bytes.Index(raw[1:], []byte{0x1b}); next_esc_i >= 0 {
--- internal/vt/wchar.go
+++ internal/vt/wchar.go
@@ -1,54 +1,16 @@
 package vt
 
-import (
-	"os"
-	"regexp"
-	"strconv"
-	"time"
-)
-
 var cprReq = []byte("\x1b[6n")
-var re = regexp.MustCompile(`\x1b\[\d+;(\d+)R`)
 
 func Wchar(y, x int, b []byte) int {
 	SetCursor(Sync, y, x)
 	Sync.Write(b)
 	Sync.Write(cprReq)
 
-	t0 := time.Now()
-	buf := make([]byte, 1024)
-	done := make(chan int)
-
-	go func() {
-		for {
-			n, err := os.Stdin.Read(buf)
-			if err != nil {
-				panic(err)
-			}
-
-			match := re.FindStringSubmatch(string(buf[:n]))
-
-			if match != nil {
-				x1, err := strconv.Atoi(match[1])
-				if err != nil {
-					panic(err)
-				}
-
-				done <- x1 - 1
-				return
-			}
-
-			if time.Since(t0).Milliseconds() > 10 {
-				done <- -1
-				return
-			}
-		}
-	}()
-
-	x1 := <-done
-	if x1 < 0 {
+	w := <-Pos - x
+	if w < 1 {
 		panic("Wchar error")
 	}
 
-	return x1 - x
+	return w
 }
