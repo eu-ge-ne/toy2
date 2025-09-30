b11e501245e18f8a5ed57774061a0060fc525ec8
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-09-29 21:18:42 -0400

    1

--- internal/vt/wchar.go
+++ internal/vt/wchar.go
@@ -15,8 +15,35 @@ func Wchar(y, x int, b []byte) int {
 	Sync.Write(b)
 	Sync.Write(cprReq)
 
+	t0 := time.Now()
+	buf := make([]byte, 1024)
 	done := make(chan int)
-	go measure(done)
+
+	go func() {
+		for {
+			n, err := os.Stdin.Read(buf)
+			if err != nil {
+				panic(err)
+			}
+
+			match := re.FindStringSubmatch(string(buf[:n]))
+
+			if match != nil {
+				x1, err := strconv.Atoi(match[1])
+				if err != nil {
+					panic(err)
+				}
+
+				done <- x1 - 1
+				return
+			}
+
+			if time.Since(t0).Milliseconds() > 10 {
+				done <- -1
+				return
+			}
+		}
+	}()
 
 	x1 := <-done
 	if x1 < 0 {
@@ -24,33 +51,4 @@ func Wchar(y, x int, b []byte) int {
 	}
 
 	return x1 - x
-}
-
-func measure(done chan<- int) {
-	t0 := time.Now()
-	buf := make([]byte, 1024)
-
-	for {
-		n, err := os.Stdin.Read(buf)
-		if err != nil {
-			panic(err)
-		}
-
-		match := re.FindStringSubmatch(string(buf[:n]))
-
-		if match != nil {
-			x1, err := strconv.Atoi(match[1])
-			if err != nil {
-				panic(err)
-			}
-
-			done <- x1 - 1
-			return
-		}
-
-		if time.Since(t0).Milliseconds() > 10 {
-			done <- -1
-			return
-		}
-	}
 }
