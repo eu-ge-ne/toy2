85dc0d27ebd949d8a5a2e4b7aaa11a09839b9eeb
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2025-09-30 03:16:36 -0400

    feat: HOME/END (#26)

---
+++ internal/editor/h_end.go
@@ -0,0 +1,23 @@
+package editor
+
+import (
+	"github.com/eu-ge-ne/toy2/internal/key"
+)
+
+type EndHandler struct {
+	editor *Editor
+}
+
+func (h *EndHandler) Match(key key.Key) bool {
+	switch {
+	case key.Name == "END":
+		return true
+	case key.Name == "RIGHT" && key.Super:
+		return true
+	}
+	return false
+}
+
+func (h *EndHandler) Handle(key key.Key) bool {
+	return h.editor.Cursor.End(key.Shift)
+}
