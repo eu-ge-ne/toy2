dfcd6330eed4b4f315841d18aff4d2e1e5514aaa
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-09-29 21:06:40 -0400

    fix: vt.Wchar timeout

--- internal/vt/wchar.go
+++ internal/vt/wchar.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"time"
 )
 
 var cprReq = []byte("\x1b[6n")
@@ -21,25 +22,31 @@ func Wchar(y, x int, b []byte) int {
 }
 
 func measure(done chan<- int) {
+	t0 := time.Now()
 	buf := make([]byte, 1024)
 
 	for {
 		n, err := os.Stdin.Read(buf)
+
 		if err != nil {
 			panic(err)
 		}
 
 		match := re.FindStringSubmatch(string(buf[:n]))
-		if match == nil {
-			continue
+
+		if match != nil {
+			x, err := strconv.Atoi(match[1])
+			if err != nil {
+				panic(err)
+			}
+
+			w := x - 1
+			done <- w
+			return
 		}
 
-		x, err := strconv.Atoi(match[1])
-		if err != nil {
-			panic(err)
+		if time.Since(t0).Milliseconds() > 10 {
+			panic("Wchar timeout")
 		}
-
-		done <- x - 1
-		return
 	}
 }
