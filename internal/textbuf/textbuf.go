3234fd9e370169ca02ed0cfca3bbff33f970a446
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-10-06 17:02:57 -0400

    1

--- internal/textbuf/textbuf.go
+++ internal/textbuf/textbuf.go
@@ -63,3 +63,45 @@ func (tb *TextBuf) Reset(text string) {
 func (tb *TextBuf) Validate() {
 	tb.tree.Root.Validate()
 }
+
+func (tb TextBuf) posToIndex(ln, col int) (int, bool) {
+	i, ok := tb.findLineStart(ln)
+
+	if !ok {
+		return 0, false
+	}
+
+	return i + col, true
+}
+
+func (tb TextBuf) findLineStart(ln int) (int, bool) {
+	if ln == 0 {
+		return 0, true
+	}
+
+	eolIndex := ln - 1
+	x := tb.tree.Root
+	i := 0
+
+	for x != node.NIL {
+		if eolIndex < x.Left.TotalEolsLen {
+			x = x.Left
+			continue
+		}
+
+		eolIndex -= x.Left.TotalEolsLen
+		i += x.Left.TotalLen
+
+		if eolIndex < x.EolsLen {
+			buf := tb.content.Table[x.PieceIndex]
+			eolEnd := buf.Eols[x.EolsStart+eolIndex].End
+			return i + eolEnd - x.Start, true
+		}
+
+		eolIndex -= x.EolsLen
+		i += x.Len
+		x = x.Right
+	}
+
+	return 0, false
+}
