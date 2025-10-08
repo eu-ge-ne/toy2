1f26f18ed267772b1c27c5315f9bffc999fcfd81
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-10-07 22:17:33 -0400

    1

--- internal/editor/syntax/syntax.go
+++ internal/editor/syntax/syntax.go
@@ -30,26 +30,28 @@ func (s *Syntax) SetLanguage() {
 }
 
 func (s *Syntax) Delete(startLn, startCol, endLn, endCol int) {
-	startByte, ok := s.buffer.Index(startLn, startCol)
-	if !ok {
-		panic("in Syntax.Delete")
-	}
+	/*
+		startByte, ok := s.buffer.Index(startLn, startCol)
+		if !ok {
+			panic("in Syntax.Delete")
+		}
 
-	oldEndByte, ok := s.buffer.Index(endLn, endCol)
-	if !ok {
-		panic("in Syntax.Delete")
-	}
+		oldEndByte, ok := s.buffer.Index(endLn, endCol)
+		if !ok {
+			panic("in Syntax.Delete")
+		}
 
-	s.tree.Edit(&treeSitter.InputEdit{
-		StartByte:      uint(startByte),
-		OldEndByte:     uint(oldEndByte),
-		NewEndByte:     uint(startByte + 1),
-		StartPosition:  treeSitter.NewPoint(uint(startLn), uint(startCol)),
-		OldEndPosition: treeSitter.NewPoint(uint(endLn), uint(endCol)),
-		NewEndPosition: treeSitter.NewPoint(uint(startLn), uint(startCol+1)),
-	})
+		s.tree.Edit(&treeSitter.InputEdit{
+			StartByte:      uint(startByte),
+			OldEndByte:     uint(oldEndByte),
+			NewEndByte:     uint(startByte + 1),
+			StartPosition:  treeSitter.NewPoint(uint(startLn), uint(startCol)),
+			OldEndPosition: treeSitter.NewPoint(uint(endLn), uint(endCol)),
+			NewEndPosition: treeSitter.NewPoint(uint(startLn), uint(startCol+1)),
+		})
 
-	s.parse()
+		s.parse()
+	*/
 }
 
 func (s *Syntax) Insert(startLn, startCol int, text string) {
