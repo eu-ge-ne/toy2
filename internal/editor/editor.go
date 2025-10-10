cf22a44208b15086345553d91ad8e5ff8d269a21
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2025-10-10 01:25:43 -0400

    1

--- internal/editor/editor.go
+++ internal/editor/editor.go
@@ -1,9 +1,12 @@
 package editor
 
 import (
+	"io"
 	"math"
+	"os"
 	"slices"
 	"time"
+	"unicode/utf8"
 
 	"github.com/eu-ge-ne/toy2/internal/editor/cursor"
 	"github.com/eu-ge-ne/toy2/internal/editor/handler"
@@ -166,4 +169,55 @@ func (ed *Editor) HandleKey(key key.Key) bool {
 	}
 
 	return r
+}
+
+func (ed *Editor) Load(filePath string) error {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	buf := make([]byte, 1024*1024*64)
+
+	for {
+		bytesRead, err := f.Read(buf)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		chunk := buf[:bytesRead]
+
+		if !utf8.Valid(chunk) {
+			panic("invalid utf8 chunk")
+		}
+
+		ed.buffer.Append(string(chunk))
+	}
+
+	ed.syntax.Reset()
+
+	return nil
+}
+
+func (ed *Editor) Save(filePath string) error {
+	f, err := os.OpenFile(filePath, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	for text := range ed.buffer.Iter() {
+		_, err := f.WriteString(text)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
