a572daee18e4941b778b6e56023a794bed9dca6e
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2025-10-13 00:40:25 -0400

    1

--- internal/editor/syntax/syntax.go
+++ internal/editor/syntax/syntax.go
@@ -9,11 +9,15 @@ import (
 	"github.com/eu-ge-ne/toy2/internal/textbuf"
 
 	treeSitter "github.com/tree-sitter/go-tree-sitter"
+	treeSitterJs "github.com/tree-sitter/tree-sitter-javascript/bindings/go"
 	treeSitterTs "github.com/tree-sitter/tree-sitter-typescript/bindings/go"
 )
 
-//go:embed highlights.scm
-var scmHighlights string
+//go:embed js/highlights.scm
+var scmJsHighlights string
+
+//go:embed ts/highlights.scm
+var scmTsHighlights string
 
 type Syntax struct {
 	buffer          *textbuf.TextBuf
@@ -37,13 +41,15 @@ func New(buffer *textbuf.TextBuf) *Syntax {
 	s.parser = treeSitter.NewParser()
 	log(s.parser)
 
-	lang := treeSitter.NewLanguage(treeSitterTs.LanguageTypescript())
-	err := s.parser.SetLanguage(lang)
+	treeSitter.NewLanguage(treeSitterJs.Language())
+	langTs := treeSitter.NewLanguage(treeSitterTs.LanguageTypescript())
+
+	err := s.parser.SetLanguage(langTs)
 	if err != nil {
 		panic(err)
 	}
 
-	queryHighlights, err0 := treeSitter.NewQuery(lang, scmHighlights)
+	queryHighlights, err0 := treeSitter.NewQuery(langTs, scmJsHighlights+scmTsHighlights)
 	if err0 != nil {
 		panic(err0)
 	}
@@ -78,33 +84,6 @@ func (s *Syntax) Delete(startLn, startCol, endLn, endCol int) {
 func (s *Syntax) Insert(startLn, startCol, endLn, endCol int) {
 	if s != nil {
 		s.edits <- edit{editKindInsert, startLn, startCol, endLn, endCol}
-	}
-}
-
-func (s *Syntax) Highlight(startLn, startCol, endLn, endCol int) {
-	if s != nil {
-		qc := treeSitter.NewQueryCursor()
-		defer qc.Close()
-
-		f, err := os.OpenFile("tmp/highlight.log", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
-		if err != nil {
-			panic(err)
-		}
-		defer f.Close()
-
-		matches := qc.Matches(s.queryHighlights, s.tree.RootNode(), []byte(s.buffer.All()))
-
-		for match := matches.Next(); match != nil; match = matches.Next() {
-			for _, capture := range match.Captures {
-				fmt.Fprintf(f,
-					"Match %d, Capture %d (%s): %s\n",
-					match.PatternIndex,
-					capture.Index,
-					s.queryHighlights.CaptureNames()[capture.Index],
-					capture.Node.Utf8Text([]byte(s.buffer.All())),
-				)
-			}
-		}
 	}
 }
 
@@ -168,6 +147,8 @@ func (s *Syntax) parseTree() {
 
 	s.tree = newTree
 	s.isDirty = false
+
+	s.highlight()
 }
 
 func (s *Syntax) editTree(e edit) {
@@ -179,4 +160,31 @@ func (s *Syntax) editTree(e edit) {
 	s.tree.Edit(&p)
 
 	s.isDirty = true
+}
+
+func (s *Syntax) highlight() {
+	if s != nil {
+		qc := treeSitter.NewQueryCursor()
+		defer qc.Close()
+
+		f, err := os.OpenFile("tmp/highlight.log", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+
+		matches := qc.Matches(s.queryHighlights, s.tree.RootNode(), []byte(s.buffer.All()))
+
+		for match := matches.Next(); match != nil; match = matches.Next() {
+			for _, capture := range match.Captures {
+				fmt.Fprintf(f,
+					"Match %d, Capture %d (%s): %s\n",
+					match.PatternIndex,
+					capture.Index,
+					s.queryHighlights.CaptureNames()[capture.Index],
+					capture.Node.Utf8Text([]byte(s.buffer.All())),
+				)
+			}
+		}
+	}
 }
