51f68b9178f2dad2361efc1478db11a8dc3c683e
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2025-10-18 15:00:04 -0400

    1

--- internal/editor/syntax/edit.go
+++ internal/editor/syntax/edit.go
@@ -1,11 +1,5 @@
 package syntax
 
-import (
-	"fmt"
-
-	treeSitter "github.com/tree-sitter/go-tree-sitter"
-)
-
 type editReq struct {
 	kind editKind
 	ln0  int
@@ -20,48 +14,3 @@ const (
 	editKindDelete editKind = iota
 	editKindInsert
 )
-
-func (s *Syntax) handleEditReq(req editReq) {
-	i0, ok := s.buffer.Index(req.ln0, req.col0)
-	if !ok {
-		panic(fmt.Sprintf("in Syntax.handleOp: %v", req))
-	}
-
-	i1, ok := s.buffer.Index(req.ln1, req.col1)
-	if !ok {
-		panic(fmt.Sprintf("in Syntax.handleOp: %v", req))
-	}
-
-	col0i, ok := s.buffer.ColIndex(req.ln0, req.col0)
-	if !ok {
-		panic(fmt.Sprintf("in Syntax.handleOp: %v", req))
-	}
-
-	col1i, ok := s.buffer.ColIndex(req.ln1, req.col1)
-	if !ok {
-		panic(fmt.Sprintf("in Syntax.handleOp: %v", req))
-	}
-
-	var ed treeSitter.InputEdit
-
-	switch req.kind {
-	case editKindDelete:
-		ed.StartByte = uint(i0)
-		ed.OldEndByte = uint(i1)
-		ed.NewEndByte = uint(i0 + 1)
-		ed.StartPosition = treeSitter.NewPoint(uint(req.ln0), uint(col0i))
-		ed.OldEndPosition = treeSitter.NewPoint(uint(req.ln1), uint(col1i))
-		ed.NewEndPosition = treeSitter.NewPoint(uint(req.ln0), uint(col0i+1))
-	case editKindInsert:
-		ed.StartByte = uint(i0)
-		ed.OldEndByte = uint(i0 + 1)
-		ed.NewEndByte = uint(i1)
-		ed.StartPosition = treeSitter.NewPoint(uint(req.ln0), uint(col0i))
-		ed.OldEndPosition = treeSitter.NewPoint(uint(req.ln0), uint(col0i+1))
-		ed.NewEndPosition = treeSitter.NewPoint(uint(req.ln1), uint(col1i))
-	}
-
-	s.tree.Edit(&ed)
-
-	s.updateTree()
-}
--- internal/editor/syntax/syntax.go
+++ internal/editor/syntax/syntax.go
@@ -4,12 +4,14 @@ import (
 	_ "embed"
 	"fmt"
 	"os"
+	"slices"
 	"time"
 
 	treeSitter "github.com/tree-sitter/go-tree-sitter"
 	_ "github.com/tree-sitter/tree-sitter-javascript/bindings/go"
 	treeSitterTs "github.com/tree-sitter/tree-sitter-typescript/bindings/go"
 
+	"github.com/eu-ge-ne/toy2/internal/std"
 	"github.com/eu-ge-ne/toy2/internal/textbuf"
 )
 
@@ -118,16 +120,14 @@ func (s *Syntax) run() {
 
 const maxChunkLen = 1024 * 16
 
-func (s *Syntax) updateTree() {
-	started := time.Now()
-
-	f, err := os.OpenFile("tmp/syntax-update.log", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
+func (s *Syntax) handleHighlightReq(req highlightReq) {
+	f, err := os.OpenFile("tmp/syntax-highlight.log", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
-	//fmt.Fprintf(f, "counter %d\n", s.counter)
-	//fmt.Fprintf(f, "ranges %d\n", s.ranges)
+
+	updateStarted := time.Now()
 
 	//s.parser.SetIncludedRanges(s.ranges)
 	//maxChunkLen := int(s.ranges[0].EndByte - s.ranges[0].StartByte)
@@ -143,5 +143,131 @@ func (s *Syntax) updateTree() {
 	s.tree.Close()
 	s.tree = t
 
-	fmt.Fprintf(f, "elapsed %v\n", time.Since(started))
+	fmt.Fprintf(f, "update: elapsed %v\n", time.Since(updateStarted))
+
+	highlightStarted := time.Now()
+
+	ln0 := max(0, req.ln0)
+	ln1 := min(s.buffer.LineCount(), req.ln1)
+	start, _ := s.buffer.LnIndex(ln0)
+	end, _ := s.buffer.LnIndex(ln1)
+	startPoint := treeSitter.NewPoint(uint(ln0), 0)
+	endPoint := treeSitter.NewPoint(uint(ln1), 0)
+
+	if s.buffer.Count() > len(s.text) {
+		s.text = make([]byte, s.buffer.Count())
+	}
+	copy(s.text[start:end], std.IterToStr(s.buffer.Read(start, end)))
+
+	qc := treeSitter.NewQueryCursor()
+	defer qc.Close()
+
+	qc.SetPointRange(startPoint, endPoint)
+	capts := qc.Captures(s.query, s.tree.RootNode(), s.text)
+
+	var (
+		span         HighlightSpan
+		spanCaptures []int
+	)
+
+	match, captIdx := capts.Next()
+	if match != nil {
+		span = HighlightSpan{
+			Start: int(match.Captures[captIdx].Node.StartByte()),
+			End:   int(match.Captures[captIdx].Node.EndByte()),
+			Color: CharFgColorUndefined,
+		}
+		spanCaptures = make([]int, 0, 5)
+	}
+
+	for ; match != nil; match, captIdx = capts.Next() {
+		capt := match.Captures[captIdx]
+
+		fmt.Fprintf(f,
+			"highlight: %v:%v %s (%s, %d, %d)\n",
+			capt.Node.StartPosition(),
+			capt.Node.EndPosition(),
+			capt.Node.Utf8Text(s.text),
+			s.query.CaptureNames()[capt.Index],
+			match.PatternIndex,
+			capt.Index,
+		)
+
+		start := int(capt.Node.StartByte())
+		end := int(capt.Node.EndByte())
+
+		if span.Start != start || span.End != end {
+			req.spans <- span
+			span = HighlightSpan{
+				Start: start,
+				End:   end,
+				Color: CharFgColorUndefined,
+			}
+			spanCaptures = make([]int, 0, 5)
+		}
+
+		spanCaptures = append(spanCaptures, int(capt.Index))
+
+		if slices.Contains(spanCaptures, 0 /*variable*/) {
+			span.Color = CharFgColorVariable
+		} else if slices.Contains(spanCaptures, 18 /*keyword*/) {
+			span.Color = CharFgColorKeyword
+		} else if slices.Contains(spanCaptures, 9 /*comment*/) {
+			span.Color = CharFgColorComment
+		} else {
+			span.Color = CharFgColorUndefined
+		}
+	}
+
+	req.spans <- span
+	close(req.spans)
+
+	fmt.Fprintf(f, "highlight: elapsed %v\n", time.Since(highlightStarted))
+}
+
+func (s *Syntax) handleEditReq(req editReq) {
+	if s.tree == nil {
+		return
+	}
+
+	i0, ok := s.buffer.Index(req.ln0, req.col0)
+	if !ok {
+		panic(fmt.Sprintf("in Syntax.handleOp: %v", req))
+	}
+
+	i1, ok := s.buffer.Index(req.ln1, req.col1)
+	if !ok {
+		panic(fmt.Sprintf("in Syntax.handleOp: %v", req))
+	}
+
+	col0i, ok := s.buffer.ColIndex(req.ln0, req.col0)
+	if !ok {
+		panic(fmt.Sprintf("in Syntax.handleOp: %v", req))
+	}
+
+	col1i, ok := s.buffer.ColIndex(req.ln1, req.col1)
+	if !ok {
+		panic(fmt.Sprintf("in Syntax.handleOp: %v", req))
+	}
+
+	var ed treeSitter.InputEdit
+
+	switch req.kind {
+	case editKindDelete:
+		ed.StartByte = uint(i0)
+		ed.OldEndByte = uint(i1)
+		ed.NewEndByte = uint(i0 + 1)
+		ed.StartPosition = treeSitter.NewPoint(uint(req.ln0), uint(col0i))
+		ed.OldEndPosition = treeSitter.NewPoint(uint(req.ln1), uint(col1i))
+		ed.NewEndPosition = treeSitter.NewPoint(uint(req.ln0), uint(col0i+1))
+	case editKindInsert:
+		ed.StartByte = uint(i0)
+		ed.OldEndByte = uint(i0 + 1)
+		ed.NewEndByte = uint(i1)
+		ed.StartPosition = treeSitter.NewPoint(uint(req.ln0), uint(col0i))
+		ed.OldEndPosition = treeSitter.NewPoint(uint(req.ln0), uint(col0i+1))
+		ed.NewEndPosition = treeSitter.NewPoint(uint(req.ln1), uint(col1i))
+	}
+
+	s.tree.Edit(&ed)
 }
