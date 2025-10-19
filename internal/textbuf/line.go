d675274d5c20e7a30317ef9c267aa4c439376630
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2025-10-19 19:56:56 -0400

    1

--- internal/textbuf/line.go
+++ internal/textbuf/line.go
@@ -7,10 +7,10 @@ import (
 	"github.com/eu-ge-ne/toy2/internal/grapheme"
 )
 
-func (buf *TextBuf) IterLine(ln int, extra bool, iterStart, iterEnd int) iter.Seq[grapheme.IterCell] {
+func (buf *TextBuf) ReadLine(ln int) iter.Seq[string] {
 	start, ok := buf.LnIndex(ln)
 	if !ok {
-		return func(yield func(grapheme.IterCell) bool) {}
+		return func(yield func(string) bool) {}
 	}
 
 	end, ok := buf.LnIndex(ln + 1)
@@ -18,6 +18,10 @@ func (buf *TextBuf) IterLine(ln int, extra bool, iterStart, iterEnd int) iter.Se
 		end = math.MaxInt
 	}
 
+	return buf.Read(start, end)
+}
+
+func (buf *TextBuf) IterLine(ln int, extra bool, iterStart, iterEnd int) iter.Seq[grapheme.IterCell] {
 	opts := grapheme.IterOptions{
 		WcharY:    buf.MeasureY,
 		WcharX:    buf.MeasureX,
@@ -27,5 +31,5 @@ func (buf *TextBuf) IterLine(ln int, extra bool, iterStart, iterEnd int) iter.Se
 		End:       iterEnd,
 	}
 
-	return grapheme.Graphemes.IterString(buf.Read(start, end), opts)
+	return grapheme.Graphemes.IterString(buf.ReadLine(ln), opts)
 }
