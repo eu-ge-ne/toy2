5cc0417361b7d3a89be5fd57eba762a88531e401
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2025-10-21 20:52:06 -0400

    1

--- internal/editor/render/render.go
+++ internal/editor/render/render.go
@@ -130,7 +130,7 @@ func (r *Render) Render() {
 
 func (r *Render) initHighlight() {
 	start, _ := r.buffer.Pos(r.ScrollLn, 0)
-	end := r.buffer.PosNear(r.ScrollLn+r.area.H-1, math.MaxInt)
+	end := r.buffer.PosMax(r.ScrollLn+r.area.H-1, math.MaxInt)
 	r.hlSpans = r.syntax.Highlight(start.Idx, end.Idx)
 
 	r.hlSpan = syntax.Span{StartIdx: -1, EndIdx: -1}
