2788b661cc154345d6453b69b1400b0054902566
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2025-10-23 17:35:31 -0400

    1

--- internal/editor/syntax/syntax.go
+++ internal/editor/syntax/syntax.go
@@ -2,6 +2,7 @@ package syntax
 
 import (
 	_ "embed"
+	"fmt"
 	"os"
 
 	treeSitter "github.com/tree-sitter/go-tree-sitter"
@@ -23,19 +24,13 @@ type Syntax struct {
 	query  *treeSitter.Query
 	tree   *treeSitter.Tree
 
-	close     chan struct{}
-	highlight chan highlightReq
-
-	text []byte
-	log  *os.File
+	log *os.File
 }
 
 func New(buffer *textbuf.TextBuf) *Syntax {
 	s := Syntax{
-		buffer:    buffer,
-		parser:    treeSitter.NewParser(),
-		close:     make(chan struct{}),
-		highlight: make(chan highlightReq),
+		buffer: buffer,
+		parser: treeSitter.NewParser(),
 	}
 
 	//Log(s.parser)
@@ -52,46 +47,16 @@ func New(buffer *textbuf.TextBuf) *Syntax {
 	}
 	s.query = query
 
-	s.run()
-
-	return &s
-}
-
-func (s *Syntax) Close() {
-	if s != nil {
-		s.close <- struct{}{}
-	}
-}
-
-func (s *Syntax) Restart() {
-	if s != nil {
-		s.Close()
-		s.run()
-	}
-}
-
-func (s *Syntax) run() {
 	f, err := os.OpenFile("tmp/syntax.log", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
 	if err != nil {
 		panic(err)
 	}
 	s.log = f
 
-	go func() {
-		for {
-			select {
-			case <-s.close:
-				s.handleClose()
-				return
-
-			case req := <-s.highlight:
-				s.handleHighlight(req)
-			}
-		}
-	}()
+	return &s
 }
 
-func (s *Syntax) handleClose() {
+func (s *Syntax) Close() {
 	if s.log != nil {
 		s.log.Close()
 		s.log = nil
@@ -99,4 +64,79 @@ func (s *Syntax) handleClose() {
 
 	s.tree.Close()
 	s.tree = nil
+}
+
+func (s *Syntax) Delete(change textbuf.Change) {
+	if s == nil || s.tree == nil {
+		return
+	}
+
+	var e treeSitter.InputEdit
+
+	e.StartByte = uint(change.Start.Idx)
+	e.OldEndByte = uint(change.End.Idx)
+
+	e.StartPosition.Row = uint(change.Start.Ln)
+	e.StartPosition.Column = uint(change.Start.ColIdx)
+	e.OldEndPosition.Row = uint(change.End.Ln)
+	e.OldEndPosition.Column = uint(change.End.ColIdx)
+
+	e.NewEndByte = e.StartByte
+	e.NewEndPosition = e.StartPosition
+
+	s.tree.Edit(&e)
+
+	fmt.Fprintf(s.log, "delete: change %+v\n", change)
+	fmt.Fprintf(s.log, "delete: e %+v\n", e)
+}
+
+func (s *Syntax) Insert(change textbuf.Change) {
+	if s == nil || s.tree == nil {
+		return
+	}
+
+	var e treeSitter.InputEdit
+
+	e.StartByte = uint(change.Start.Idx)
+	e.NewEndByte = uint(change.End.Idx)
+
+	e.StartPosition.Row = uint(change.Start.Ln)
+	e.StartPosition.Column = uint(change.Start.ColIdx)
+	e.NewEndPosition.Row = uint(change.End.Ln)
+	e.NewEndPosition.Column = uint(change.End.ColIdx)
+
+	e.OldEndByte = e.StartByte
+	e.OldEndPosition = e.StartPosition
+
+	s.tree.Edit(&e)
+
+	fmt.Fprintf(s.log, "insert: change %+v\n", change)
+	fmt.Fprintf(s.log, "insert: e %+v\n", e)
+}
+
+func (s *Syntax) Highlight(startLn, endLn int) *Highlighter {
+	return NewHighlighter(s, startLn, endLn)
+}
+
+func (s *Syntax) parse(startPos, endPos textbuf.Pos) {
+	s.parser.SetIncludedRanges([]treeSitter.Range{{
+		StartByte:  uint(startPos.Idx),
+		EndByte:    uint(endPos.Idx),
+		StartPoint: treeSitter.NewPoint(uint(startPos.Ln), uint(startPos.ColIdx)),
+		EndPoint:   treeSitter.NewPoint(uint(endPos.Ln), uint(endPos.ColIdx)),
+	}})
+
+	oldTree := s.tree
+
+	s.tree = s.parser.ParseWithOptions(func(i int, p treeSitter.Point) []byte {
+		text := s.buffer.Chunk(i)
+
+		if len(text) > maxChunkLen {
+			text = text[0:maxChunkLen]
+		}
+
+		fmt.Fprintf(s.log, "parse: reading chunk %d, %+v, %d\n", i, p, len(text))
+
+		return []byte(text)
+	}, oldTree, nil)
 }
