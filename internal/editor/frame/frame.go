5ff80a7684f9703f010a87fcee6c032419081382
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2025-11-03 17:12:38 -0500

    refactor: Frame (#73)

--- internal/editor/frame/frame.go
+++ internal/editor/frame/frame.go
@@ -157,35 +157,33 @@ func (fr *Frame) scrollV() {
 	}
 
 	// Below?
-
 	if delta > fr.area.H {
-		fr.scrollLn = fr.cursor.Ln - fr.area.H
 		delta = fr.area.H
+		fr.scrollLn = fr.cursor.Ln - delta
 	}
 
-	heights := make([]int, delta+1)
+	hh := make([]int, delta+1)
+	h := 0
 
-	for i := 0; i < len(heights); i += 1 {
-		heights[i] = 1
-
+	for i := 0; i < len(hh); i += 1 {
 		for cell := range grapheme.Wrap(fr.buffer.LineGraphemes(fr.scrollLn+i), fr.wrapWidth, false) {
-			if cell.Col > 0 && cell.WrapCol == 0 {
-				heights[i] += 1
+			if cell.WrapCol == 0 {
+				hh[i] += 1
+				h += 1
 			}
 		}
 	}
 
 	i := 0
-	height := std.Sum(heights)
 
-	for height > fr.area.H {
-		height -= heights[i]
+	for h > fr.area.H {
+		h -= hh[i]
 		fr.scrollLn += 1
 		i += 1
 	}
 
-	for i < len(heights)-1 {
-		fr.cursorY += heights[i]
+	for i < len(hh)-1 {
+		fr.cursorY += hh[i]
 		i += 1
 	}
 }
